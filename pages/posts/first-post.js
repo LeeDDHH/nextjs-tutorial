c24602c59c2c09d35161e07e7bacedceafc35cbc
DongHeon Lee <niche3600@gmail.com>
2021-03-02 08:06:47 +0900

    Imageの使い方

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 const FirstPost = () => {
   return (
@@ -9,6 +10,12 @@ const FirstPost = () => {
           <a>Back 2 Home</a>
         </Link>
       </h2>
+      <Image
+        src="/images/calm-place.jpg"
+        height={144}
+        width={432}
+        alt="pic"
+      />
     </>
   )
 }
