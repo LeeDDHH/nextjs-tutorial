f250f98cff8c4d2d3ab72ff5f7e2c61ca43e8c02
DongHeon Lee <niche3600@gmail.com>
2021-03-06 08:08:50 +0900

    YAMLパーサーを使って記事一覧を表示する

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,36 @@ import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Hello I'm <strong>DongHeon</strong>. I'm a software engineer in Tokyo. You can contact me on <Link href="https://twitter.com/camomile_cafe"><a target="_blank">Twitter</a></Link>.</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">Next.js tutorial</a>.)
-        </p>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
